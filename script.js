80241bb1e26ee56d83d9364b1b9b51c001a36c72
MANOJ-02460 <131439674+MANOJ-02460@users.noreply.github.com>
2026-07-10 19:12:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
-
 const slider = document.querySelector(".items");
+
 let isDown = false;
 let startX = 0;
 let scrollLeft = 0;
 
-slider.addEventListener("mousedown",(e)=>{
-	if(e.which !== 1) return;
-	isDown=true;
-	slider.classList.add("active");
-	startX = e.pageX;
-	scrollLeft = slider.scrollLeft;
+slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    slider.classList.add("active");
+
+    startX = e.pageX;
+    scrollLeft = slider.scrollLeft;
 });
 
-slider.addEventListener("mousemove",(e)=>{
-	if(!isDown) return;
+document.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
 
-	e.preventDefault();
-	const walk = e.pageX - startX;
+    e.preventDefault();
+
+    const walk = e.pageX - startX;
     slider.scrollLeft = scrollLeft - walk;
 });
 
-slider.addEventListener("mouseup",()=>{
-	isDown=false;
-	slider.classList.remove("active");
+document.addEventListener("mouseup", () => {
+    isDown = false;
+    slider.classList.remove("active");
 });
 
-slider.addEventListener("mouseleave",()=>{
-	isDown=false;
-	slider.classList.remove("active");
-})
+slider.addEventListener("mouseleave", () => {
+    isDown = false;
+    slider.classList.remove("active");
+});
